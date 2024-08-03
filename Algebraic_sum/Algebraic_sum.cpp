bc878ad58a4da047de99a11ea088c2f1b98c7a2e
B1oodyAnge1 <romsm@bk.ru>
2024-08-03 18:32:10 +0300

    The task is completed

--- Algebraic_sum/Algebraic_sum.cpp
+++ Algebraic_sum/Algebraic_sum.cpp
@@ -1,7 +1,33 @@
 ﻿#include <iostream>
+#include <string>
+#include <regex>
+#include <cmath>
+
+
+//Найти алгебраическую сумму для выражения : 1k + 2k + 3k + … + Nk.N и степень k вводит пользователь.
+
+int inputInt(std::string);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "ru");
+    int N = inputInt("Введите N: "), k = inputInt("Введите степень k: "),rez = 0;
+    for (int i = 0; i < N; i++) {
+        rez += pow(i + 1,k);
+    }
+    std::cout << "Сумма: " << rez;
+    return 0;
 }
 
+int inputInt(std::string textReq = "Enter an integer number") { // textReq текстовый запрос у пользователя
+    std::string number;
+    std::regex reg("^[0-9]"); //регулярное выражение
+    while (true)
+    {
+        std::cout << textReq << std::endl;
+        std::cin >> number;
+        if (std::regex_search(number, reg)) {
+            return stoi(number); //преобразование string в int. С++ 11
+        }
+    }
+}
